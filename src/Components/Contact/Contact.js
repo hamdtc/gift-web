72a648b5c2a50bf2a759a828cc04b658476ff244
vani <vanisravanthi87@gmail.com>
2023-07-03 08:30:09 +0530

    upadates

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,3 +1,39 @@
 export default function Contact( ) {
-    return <h1>Contact</h1>
+    return (
+        <div id="container1">
+        <div class="card">
+        <form>
+            <h2> CONTACT US</h2>
+            <p class="para">Feel free to contact us any time. We will get back to <br/>you as soon as we can.</p>
+            <input type="text" name="name" placeholder="Name" class="f1"/><br/><br/>
+            <input type="text" name="Email" placeholder="Email" class="f2"/><br/><br/>
+            <input type="text" name="Message" placeholder="Message" class="f3"/><br/><br/>
+            <button class="send" > Send</button>
+            </form>
+          
+            </div>
+
+            <div class="card2">
+                <div class="image">
+                <img src="vector4.png" class="i1"/>&nbsp;&nbsp;14 GreenRoad St. <br/><br/>
+                <img src="vector5.png" class="12"/>&nbsp;&nbsp;14 GreenRoad St.<br/><br/>
+                <img src="vector6.png" class="i3"/>&nbsp;&nbsp;avira@getinfo.com<br/><br/>
+                <img src="vector7.png" class="i4"/>&nbsp;&nbsp;Mon-Fri: 9:00 AM - 8:00 PM <br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Sat: 9:30 AM - 6:30 PM<br/><br/><br/>
+                <img src="vector8.png" class="i5"/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <img src="vector9.png" class="i6"/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <img src="vector10.png" class="i7"/>
+                </div>
+                <div class="image2">
+                <img src="img.png" />
+                </div>
+            </div>
+            
+                
+           
+
+
+
+
+    </div>
+    )
 }
