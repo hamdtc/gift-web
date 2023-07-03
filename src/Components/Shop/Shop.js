3dd09ea0b403d0b690d2b7d466aa3751d5507390
vani <vanisravanthi87@gmail.com>
2023-07-03 10:27:35 +0530

    updates

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,3 +1,33 @@
+import "./shop.css"
 export default function Shop( ) {
-    return <h1>Shop</h1>
+    return (
+
+    <div class='image'>
+       
+        <img src="./img1.png"  alt=" "  class="i1" >
+        
+        </img>
+        
+        <img src="./img2.png"  alt=" "  class="i2" >
+         
+        </img>
+        
+        <img src='./img3.png'  alt=" "  class="i3" >
+         
+        </img>
+        
+        <img src='./img4.png'  alt=" "  class="i4" >
+         
+        </img>
+          
+        <img src='./img5.png'  alt=" "  class="i5" >
+        
+        </img>
+        
+        <img src='./img6.png'  alt=" "  class="i6" >
+        
+        </img>
+        
+    </div>
+  )
 }
