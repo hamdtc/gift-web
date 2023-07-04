a0f5098a215aac7e200805a945587766a44a26db
hamdtc <mohamedtc73@gmail.com>
2023-07-04 10:54:36 +0530

    no css

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,33 +1,33 @@
 import "./Shop.css"
-export default function Shop( ) {
-    return (
+export default function Shop() {
+  return (
 
     <div class='image'>
-       
-        <img src="./image1.png"  alt=" "  class="i1" >
-        
-        </img>
-        
-        <img src="./image2.png"  alt=" "  class="i2" >
-         
-        </img>
-        
-        <img src='./image3.png'  alt=" "  class="i3" >
-         
-        </img>
-        
-        <img src='./image4.png'  alt=" "  class="i4" >
-         
-        </img>
-          
-        <img src='./image5.png'  alt=" "  class="i5" >
-        
-        </img>
-        
-        <img src='./image6.png'  alt=" "  class="i6" >
-        
-        </img>
-        
+
+      <img src="./image1.png" alt=" " class="i1" >
+
+      </img>
+
+      <img src="./image2.png" alt=" " class="i2" >
+
+      </img>
+
+      <img src='./image3.png' alt=" " class="i3" >
+
+      </img>
+
+      <img src='./image4.png' alt=" " class="i4" >
+
+      </img>
+
+      <img src='./image5.png' alt=" " class="i5" >
+
+      </img>
+
+      <img src='./image6.png' alt=" " class="i6" >
+
+      </img>
+
     </div>
   )
 }
