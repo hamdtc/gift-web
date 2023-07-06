7db8c7a1927343018bdccf6e119c59adc9d3cf63
vani <vanisravanthi87@gmail.com>
2023-07-06 14:32:38 +0530

    updates

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -5,28 +5,34 @@ export default function Contact( ) {
         <div className="card">
         <form>
             <h2> CONTACT US</h2>
-            <p className="para">Feel free to contact us any time. We will get back to <br/>you as soon as we can.</p>
+            <p className="para">Feel free to contact us any time. We will get back to <br/>you as soon as we can.</p><br/>
+            <form>
             <input type="text" name="name" placeholder="Name" className="f1"/><br/><br/>
             <input type="text" name="Email" placeholder="Email" className="f2"/><br/><br/>
             <input type="text" name="Message" placeholder="Message" className="f3"/><br/><br/>
             <button class="send" > Send</button>
             </form>
+            </form>
           
             </div>
 
+            
+
             <div className="card2">
+                
+               <img src="vector5.png"/> &nbsp;&nbsp;&nbsp;&nbsp;14 GreenRoad St.<br/><br/><br/>
+               <img src="vector6.png"/>&nbsp;&nbsp;&nbsp;&nbsp;14 GreenRoad St. <br/><br/><br/>
+               <img src="vector7.png"/>&nbsp;&nbsp;&nbsp;&nbsp;avira@getinfo.com<br/><br/><br/>
+               <img src="vector8.png"/>&nbsp;&nbsp;&nbsp;&nbsp;Mon-Fri: 9:00 AM - 8:00 PM <br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Sat: 9:30 AM - 6:30 PM<br/><br/><br/>
+               <img src="vector9.png"/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+               <img src="vector10.png"/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+               <img src='vector11.png'/>
+               
+               </div>
+
                 <div className="image">
-                <img src="vector4.png" className="i1"/>&nbsp;&nbsp;14 GreenRoad St. <br/><br/>
-                <img src="vector5.png" className="12"/>&nbsp;&nbsp;14 GreenRoad St.<br/><br/>
-                <img src="vector6.png" className="i3"/>&nbsp;&nbsp;avira@getinfo.com<br/><br/>
-                <img src="vector7.png" className="i4"/>&nbsp;&nbsp;Mon-Fri: 9:00 AM - 8:00 PM <br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Sat: 9:30 AM - 6:30 PM<br/><br/><br/>
-                <img src="vector8.png" className="i5"/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <img src="vector9.png" className="i6"/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <img src="vector10.png" className="i7"/>
-                </div>
-                <div className="image2">
                 <img src="img.png" />
-                </div>
+                
             </div>
             
                 
