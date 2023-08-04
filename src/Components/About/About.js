c24d581678bb7ce043053cb0415e75b31d382cfc
vani <vanisravanthi87@gmail.com>
2023-08-04 14:22:35 +0530

    updates

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -8,27 +8,27 @@ export default function About( ) {
         <p className="para"> Avira caters to thoughtful shoppers who appreciate unique designs and top quality pieces you just can’t find elsewhere. We are constantly curating fresh new collections and looking for the next big thing our customers will love. Founded in Vienna in 2019, we are proud to be your Online Clothing Shop that you can rely on for our expert service and care. <br/><br/> 
         We are here to serve you and make sure you find the Perfect Look for every occasion. Our passion for fashion is the reason why we are here. We absolutely love what we do, and our goal is to help our customers by providing them with unique outfit and accessories that make them stand-out from the crowd.</p> <br/> <br/> <br/> <br/> <br/>
        
-       <div className="frame-about">
-        <div className="frame1-about"/>
-        <b className="about1"> 50k+</b><br/>
-        <small>Happy customers </small>
+       <div className="row">
+        <div className="col">
+        <h2> 50k+</h2>
+        <small>Happy customers </small></div>
 
-        <div className="frame2-about">
-        <b className="about2">60+</b><br/>
+        <div className="col">
+        <h2 >60+</h2>
         <small>Top Partners</small></div>
 
-        <div className="frame3-about">
-        <b className="about3"> 5+</b> <br/>
+        <div className="col">
+        <h2> 5+</h2> 
         <small>Years Of Trust </small></div>
         
-        
         </div>
+        
           
         <div  className="image-about"> 
         <img src="image.png" />
-    </div>
+     </div>
          
-          </div>
+     </div> 
          
     )
 }
